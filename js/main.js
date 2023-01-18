3424344ffd7284923d0cfb4da976609c5527cd59
Lucas Arruda Vidal <lucas.2019107665@unicap.br>
2023-01-18 14:14:38 -0300

    slides js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll:1,
+    adaptiveHeight: true,
+    autoplay: true,
+    altoplaySpeed: 5000
+
+});
